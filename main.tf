5103e68ff14e9686f380151f92325f1f3792fb83
sohye02 <sohye.park85@gmail.com>
2022-02-10 03:07:10 +0000

    delete s3

--- main.tf
+++ main.tf
@@ -44,7 +44,7 @@ module "data" {
   mariadb_instance_class   = var.mariadb_instance_class
   mariadb_master_user_name = var.mariadb_master_user_name
   mariadb_master_password  = var.mariadb_master_password
-  s3_bucket_name           = var.s3_bucket_name
+#  s3_bucket_name           = var.s3_bucket_name
   redis_port               = var.redis_port
 
   vpc_id     = module.vpc.vpc_id
